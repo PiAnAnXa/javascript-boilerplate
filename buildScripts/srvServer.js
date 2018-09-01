dfd88ade2ec8bde9ecf86cb0dda04d28799188ed
Brainsink <brainsink@gmail.com>
2018-09-02 00:31:46 +0200

    Reremov open in favor of opn

--- buildScripts/srvServer.js
+++ buildScripts/srvServer.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-import open from 'open';
+import opn from 'opn';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
@@ -46,6 +46,6 @@ app.listen(port, (err) => {
     if(err) {
         console.log(err);
     } else {
-        open('http://localhost:' + port);
+        opn('http://localhost:' + port);
     }
 })
